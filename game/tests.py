1f2b7b7530b4d254f64597687a12308c0e9d1374
Carl Boström <carl.bostrom@aalto.fi>
2017-12-28 16:57:35 +0200

    add response test

--- game/tests.py
+++ game/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from game.models import Game
 
 # Create your tests here.
+
+class BuyViewTest(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        Game.create(name='name', url='url').save()
+    
+    def testResponses(self):
+        
+        game = Game.objects.all()[0]
+        pk = game.pk
+        
+        response = self.client.get(reverse('game:buy', args=[pk]))
+        self.assertEquals(response.status_code, 200)
+        response = self.client.get(reverse('game:buy', args=[pk + 1]))
+        self.assertEquals(response.status_code, 404)
