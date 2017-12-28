d709b4f8da9464c018e2dcd8dfa469ad9de3deb7
laxmi <laxmi.thebe@aalto.fi>
2017-12-28 16:46:34 +0200

    Document form

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from django.contrib.auth.models import User, Group
 
 class RegisterForm(UserCreationForm):
+    """Form to register the user. Custom form is used to collect the username
+    and email for registration"""
     email = forms.EmailField(max_length=100, help_text='Requried a valid email')
     group = forms.ModelChoiceField(queryset=Group.objects.all(), required=True)
     class Meta:
@@ -10,4 +12,6 @@ class RegisterForm(UserCreationForm):
         fields = ('username', 'email', 'group', 'password1', 'password2')
 
 class GroupChoiceForm(forms.Form):
+    """GroupChoiceForm is used to register group - basically for the user who
+    uses social login"""
     group = forms.ModelChoiceField(queryset=Group.objects.all(), required=True)
