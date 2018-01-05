673a7ce1d54581c337d931282f34f52d010a1063
laxmi <laxmi.thebe@aalto.fi>
2018-01-05 12:59:45 +0200

    Social Share + Payment Detail done

--- game/admin.py
+++ game/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Game, GamePlayed
+from .models import Game, GamePlayed, PaymentDetail
 # Register your models here.
 class GameAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Game, GameAdmin)
 admin.site.register(GamePlayed)
+admin.site.register(PaymentDetail)
