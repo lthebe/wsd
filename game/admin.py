68df9ef10d0c83daa61a477db76d07f7ecdcaf5e
Laxmi Thebe <laxmi.thebe@aalto.fi>
2018-01-06 08:21:09 +0200

    Revert "Social Share + Payment Detail done"
    This reverts commit 673a7ce1d54581c337d931282f34f52d010a1063

--- game/admin.py
+++ game/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Game, GamePlayed, PaymentDetail
+from .models import Game, GamePlayed
 # Register your models here.
 class GameAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Game, GameAdmin)
 admin.site.register(GamePlayed)
-admin.site.register(PaymentDetail)
