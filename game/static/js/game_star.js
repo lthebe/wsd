6d733a0b13e3a8f61c73abb75bf526dd629e13f4
Carl Boström <carl.bostrom@aalto.fi>
2018-01-17 15:07:26 +0200

    add responsive ratings field

---
+++ game/static/js/game_star.js
@@ -0,0 +1,16 @@
+/* global $ */
+$(document).ready(function(){
+  "use strict";
+  
+  $(".rating-star").click(function(){
+    var x = parseInt(this.id[5]);
+    $(".rating-star")
+      .filter(":lt(" + x + ")")
+      .children("p")
+      .addClass("star-full");
+    $(".rating-star")
+      .filter(":gt(" + (x - 1) + ")")
+      .children("p")
+      .removeClass("star-full");
+  });
+});
