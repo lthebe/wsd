ca2b10f9b949e4535bf232a97a96844df1a9650e
Carl Boström <carl.bostrom@aalto.fi>
2018-02-07 13:16:42 +0200

    add developed games test

--- api/tests.py
+++ api/tests.py
@@ -40,24 +40,86 @@ class UsersTest(TestCase):
         self.assertEquals(response.status_code, 200)
         
         content = self.parser.parse(BytesIO(response.content))
-        self.assertEquals(len(content), 8)
         
-        username_list = list(map(lambda user: user.username, User.objects.all()))
-        for user in content:
-            self.assertTrue(user['username'] in username_list)
-    
+        got_users = {user['username'] for user in content}
+        has_users = {user.username for user in User.objects.all()}
+        self.assertTrue(got_users == has_users)
+        
     def testDeveloperList(self):
         
         response = self.client.get(reverse('api:developer-list'), None, format='json')
         self.assertEquals(response.status_code, 200)
         
         content = self.parser.parse(BytesIO(response.content))
-        self.assertEquals(len(content), 3)
         
-        developer_list = list(map(
-            lambda user: user.username,
+        got_users = {user['username'] for user in content}
+        has_users = {user.username for user in
             Group.objects.get(name='Developer').user_set.all()
-        ))
-        for user in content:
-            self.assertTrue(user['username'] in developer_list)
+        }
+        self.assertTrue(got_users == has_users)
+        
+        #self.assertEquals(len(content), 3)
+        #
+        #developer_list = list(map(
+        #    lambda user: user.username,
+        #    Group.objects.get(name='Developer').user_set.all()
+        #))
+        #for user in content:
+        #    self.assertTrue(user['username'] in developer_list)
 
+class DevelopedGamesTest(TestCase):
+    
+    def setUp(self):
+        
+        logger.debug('DeveloperGamesTest.setUp')
+        
+        self.parser = JSONParser()
+        self.client = APIClient()
+        
+        dev_group = Group.objects.get(name='Developer')
+        
+        for i in range(4):
+            user = User.objects.create(username='dev{}'.format(i))
+            user.save()
+            dev_group.user_set.add(user)
+        
+        for i in range(4):
+            developer = User.objects.get(username='dev{}'.format(i))
+            for j in range(i):
+                Game.create(
+                    title='game{}x{}'.format(i, j),
+                    url='http://foobar.fi',
+                    developer=developer
+                ).save()
+        
+    def testGamesList(self):
+        
+        response = self.client.get(reverse('api:game-list'), None, format='json')
+        self.assertEquals(response.status_code, 200)
+        
+        content = self.parser.parse(BytesIO(response.content))
+        
+        got_games = {(game['title'], game['developer']) for game in content}
+        has_games = {
+            (game.title, game.developer.username)
+            for game in Game.objects.all()
+        }
+        self.assertTrue(got_games == has_games)
+    
+    def testDevelopedGamesList(self):
+        
+        for developer in Group.objects.get(name='Developer').user_set.all():
+            response = self.client.get(
+                reverse('api:developer-games', args=[developer.username]),
+                None, format='json'
+            )
+            self.assertEquals(response.status_code, 200)
+            
+            content = self.parser.parse(BytesIO(response.content))
+            
+            got_games = {game['title'] for game in content}
+            has_games = {
+                game.title for game in Game.objects.all().filter(developer=developer)
+            }
+            
+            self.assertTrue(got_games == has_games)
